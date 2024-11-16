8e8bae8867cf1f33a77a7c524558092e5b3f8803
Emre Uğur <46279069+Emruur@users.noreply.github.com>
2024-11-16 14:22:19 +0100

    fnl

--- GA.py
+++ GA.py
@@ -5,26 +5,71 @@ import numpy as np
 # https://pypi.org/project/ioh/
 import ioh
 from ioh import get_problem, logger, ProblemClass
+from dataclasses import dataclass
 
 budget = 5000
 
 # To make your results reproducible (not required by the assignment), you could set the random seed by
 # `np.random.seed(some integer, e.g., 42)`
 
+@dataclass
+class HyperParams:
+    pop_size: int = 1000
+    n_selections: int = 1000
+    mutation_rate: float = 0.01
+    crossover_probability: float = 0.1
+
+hyper_params = HyperParams()
+
+# Uniform Crossover
+def crossover(p1, p2):
+    return [np.random.choice([b1,b2]) for b1,b2 in zip(p1,p2)]
+
+# Standard bit mutation using mutation rate p
+def mutation(p, mutation_rate):
+    p_mutated = [b ^ 1 if np.random.rand() <= mutation_rate else b for b in p]
+    return p_mutated
+
+# Using the Fitness proportional selection
+# TODO if i'm not learning chnage me to boltzman or smth
+def mating_selection(parents, parents_f, n_selections) :   
+    parents_f_np= np.array(parents_f)
+    fitness_sum= sum(parents_f)
+    parents_pdf = parents_f_np / fitness_sum
+
+    selected_parents= []
+    for _ in range(n_selections):
+        parent_indices = np.arange(len(parents))
+        p1_idx = np.random.choice(parent_indices, p=parents_pdf)
+        p2_idx = np.random.choice(parent_indices, p=parents_pdf)
+
+        # Select the parents based on the sampled indices
+        p1 = parents[p1_idx]
+        p2 = parents[p2_idx]
+        selected_parents.append((p1,p2))
+
+    return selected_parents
+
 def studentnumber1_studentnumber2_GA(problem: ioh.problem.PBO) -> None:
     # initial_pop = ... make sure you randomly create the first population
+    parents= [np.random.randint(2, size = problem.meta_data.n_variables) for _ in range(hyper_params.pop_size)]
+
+    ## TODO i might break
+    parents_fitness= [problem(solution) for solution in parents]
+
 
     # `problem.state.evaluations` counts the number of function evaluation automatically,
     # which is incremented by 1 whenever you call `problem(x)`.
     # You could also maintain a counter of function evaluations if you prefer.
+
     while problem.state.evaluations < budget:
         # please implement the mutation, crossover, selection here
-        # .....
-        # this is how you evaluate one solution `x`
-        # f = problem(x)
-    # no return value needed
-        pass
+        selected_parents= mating_selection(parents, parents_fitness, hyper_params.n_selections)
+        offsprings= [crossover(selected_parent[0], selected_parent[1]) for selected_parent in selected_parents]
+        offsprings= [mutation(offspring, hyper_params.mutation_rate) for offspring in offsprings]
 
+        parents= offsprings
+        parents_fitness= [problem(solution) for solution in parents]
 
 def create_problem(dimension: int, fid: int) -> Tuple[ioh.problem.PBO, ioh.logger.Analyzer]:
     # Declaration of problems to be tested.
